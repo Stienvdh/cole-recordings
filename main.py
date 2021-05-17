afa7760f2bcca63dd2c67750af68164c5065b6c4
Stien Vanderhallen <stienvan@cisco.com>
2021-05-17 13:15:33 +0200

    Redirect URI, new try

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ WEBEX_MEETINGS_API_URL = "https://api.webex.com/WBXService/XMLService"
 
 webex_integration_client_id = os.environ["INT_CLIENT_ID"]
 webex_integration_client_secret= os.environ["INT_CLIENT_SECRET"]
-webex_integration_redirect_uri = f"{request.url_root}/webexoauth"
 webex_integration_scope = "spark:all meeting:schedules_write"
 
 # Flask app
@@ -55,9 +54,9 @@ def upload_file():
 # webex access token
 @app.route('/webexlogin', methods=['POST'])
 def webexlogin():
-    WEBEX_USER_AUTH_URL = WEBEX_LOGIN_API_URL + "/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}/webexoauth&response_mode=query&scope={scope}".format(
+    WEBEX_USER_AUTH_URL = WEBEX_LOGIN_API_URL + "/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&response_mode=query&scope={scope}".format(
         client_id=urllib.parse.quote(webex_integration_client_id),
-        redirect_uri=urllib.parse.quote(webex_integration_redirect_uri),
+        redirect_uri=f'{request.url_root}/webexoauth',
         scope=urllib.parse.quote(webex_integration_scope)
     )
     return redirect(WEBEX_USER_AUTH_URL)
@@ -73,7 +72,7 @@ def webexoauth():
     body = {
         'client_id': webex_integration_client_id,
         'code': webex_code,
-        'redirect_uri': webex_integration_redirect_uri,
+        'redirect_uri': f'{request.url_root}/webexoauth',
         'grant_type': 'authorization_code',
         'client_secret': webex_integration_client_secret
     }
